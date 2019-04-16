ed534ffe4fa3b59d263493a839f2620c94cae06c
Eva Demers-Brett <evademersbrett@gmail.com>
2019-04-16 17:51:57 -0400

    Set up jest, can run stuff successfully

--- in-flight/dist/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-
-var _test = require("./test.js");
-
-console.log('Hello, I am index!');
-(0, _test.aTestFunction)();
---
+++ in-flight/jest.setup.js
@@ -0,0 +1,7 @@
+import 'jest-dom/extend-expect'
+// import { configure } from 'enzyme'
+// import Adapter from 'enzyme-adapter-react-16'
+import { matchers } from 'jest-emotion'
+expect.extend(matchers)
+
+// configure({ adapter: new Adapter() })
