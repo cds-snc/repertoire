cb1e639461de43212895ff083f6a11b5a0f6c488
evadb <evademersbrett@gmail.com>
2019-04-24 17:57:24 -0400

    Add asAnchor

--- src/index.js
+++ src/index.js
@@ -13,3 +13,5 @@ export { Container, Content, CenterContent } from './container'
 export { Breadcrumb } from './breadcrumb'
 
 export { Button } from './button'
+
+export { asAnchor } from './utils/asAnchor'
---
+++ src/utils/asAnchor/index.js
@@ -0,0 +1,68 @@
+import React from 'react'
+import styled from '@emotion/styled'
+import {
+  fontSize,
+  fontWeight,
+  lineHeight,
+  space,
+  colorStyle,
+  color,
+  textAlign,
+  width,
+  height,
+  maxWidth,
+  maxHeight,
+  minHeight,
+  minWidth,
+  borders,
+} from 'styled-system'
+import { cleanProps } from '../cleanProps'
+import PropTypes from 'prop-types'
+
+export const asAnchor = AnchorType => {
+  const Anchor = props => <AnchorType {...props}>{props.children}</AnchorType>
+
+  Anchor.propTypes = {
+    children: PropTypes.any,
+  }
+
+  const StyledAnchor = styled(Anchor, {
+    shouldForwardProp: prop => cleanProps(prop),
+  })`
+    font-family: ${({ theme }) => theme.fontSans};
+    margin: 0;
+    ${fontSize};
+    ${fontWeight};
+    ${lineHeight};
+    ${colorStyle};
+    ${color};
+    ${space};
+    ${textAlign};
+    ${width};
+    ${height};
+    ${maxWidth};
+    ${maxHeight};
+    ${minHeight};
+    ${minWidth};
+    ${borders};
+  `
+
+  StyledAnchor.propTypes = {
+    ...fontSize.propTypes,
+    ...fontWeight.propTypes,
+    ...lineHeight.propTypes,
+    ...space.propTypes,
+    ...colorStyle.propTypes,
+    ...color.propTypes,
+    ...textAlign.propTypes,
+    ...width.propTypes,
+    ...height.propTypes,
+    ...maxWidth.propTypes,
+    ...maxHeight.propTypes,
+    ...minHeight.propTypes,
+    ...minWidth.propTypes,
+    ...borders.propTypes,
+  }
+
+  return StyledAnchor
+}
